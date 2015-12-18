b1fb2fe4ddc5abe610c66833af7706e7c135b857
kinggggg <liweibooooo@126.com>
2015-12-18 16:21:49 +0800

    使用resultType映射一对一关联查询结果

---
+++ src/main/java/com/zeek/mybatis3/mapper/OrdersMapperCustom.java
@@ -0,0 +1,19 @@
+package com.zeek.mybatis3.mapper;
+
+import java.util.List;
+
+import com.zeek.mybatis3.po.OrdersCustom;
+
+/**
+ * 
+ * <p>Title: OrdersMapperCustom</p>
+ * <p>Description: 订单mapper</p>
+ * @date	2015-4-23上午10:28:43
+ * @version 1.0
+ */
+public interface OrdersMapperCustom {
+	
+	//查询订单关联查询用户信息
+	public List<OrdersCustom> findOrdersUser()throws Exception;
+
+}
---
+++ src/test/java/com/zeek/mybatis/mapper/OrdersMapperCustomTest.java
@@ -0,0 +1,51 @@
+package com.zeek.mybatis.mapper;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.zeek.mybatis3.mapper.OrdersMapperCustom;
+import com.zeek.mybatis3.po.OrdersCustom;
+
+public class OrdersMapperCustomTest {
+
+	private SqlSessionFactory sqlSessionFactory;
+
+	// 此方法是在执行testFindUserById之前执行
+	@Before
+	public void setUp() throws Exception {
+		// 创建sqlSessionFactory
+
+		// mybatis配置文件
+		String resource = "SqlMapConfig.xml";
+		// 得到配置文件流
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+
+		// 创建会话工厂，传入mybatis的配置文件信息
+		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+	}
+
+	@Test
+	public void testFindOrdersUser() throws Exception {
+
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		// 创建代理对象
+		OrdersMapperCustom ordersMapperCustom = sqlSession
+				.getMapper(OrdersMapperCustom.class);
+
+		// 调用maper的方法
+		List<OrdersCustom> list = ordersMapperCustom.findOrdersUser();
+
+		System.out.println(list);
+		System.out.println(list.size());
+
+		sqlSession.close();
+	}
+
+}
